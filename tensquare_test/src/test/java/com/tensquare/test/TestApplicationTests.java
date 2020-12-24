da5a2c43e0f25a0e323e7aa7d34a2ebe6d1d480f
daixiaoyong <Dxy3262945>
2020-12-24 19:59:30 +0800

    更新docker-maven-plugin插件版本

--- tensquare_test/src/test/java/com/tensquare/test/TestApplicationTests.java
+++ tensquare_test/src/test/java/com/tensquare/test/TestApplicationTests.java
@@ -1,13 +1,17 @@
 package com.tensquare.test;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class TestApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+
     }
 
 }
